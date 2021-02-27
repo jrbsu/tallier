bca2b6f602dd03ad7c715d880880136d6b16257d
Joe Sutherland <jrbsutherland@gmail.com>
2021-02-26 18:12:12 -0800

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -5,10 +5,12 @@ $(document).ready(function () {
         dumptext = "",
         votes = [],
         candidates = [],
+        lang = "",
         voters = [],
         finalVotes = [],
         standings = [],
         candidateAmount = 0,
+        mustVote = document.querySelector('input[name="must-vote"]'),
         trimHold = "",
         voteStart = new RegExp("<vote>.*"),
         candidateStart = new RegExp("<message.*");
@@ -26,6 +28,8 @@ $(document).ready(function () {
         $('#candidate-list').html("");
         $('#votes-list').html("");
         candidateAmount = parseInt($('#candidates').val());
+        lang = $('#lang').val();
+        lang = new RegExp("<message.*lang=\"" + lang + "\">");
         
         if (!(candidateAmount > 0)) { // if we don't have an expected number of successful candidates, stop everything
             alert("Enter a candidate number please");
@@ -33,11 +37,14 @@ $(document).ready(function () {
         }
         
         dumptext = $('#dump-box').val();
+        
+        dumptext = dumptext.replace(/<SecurePoll.[\s\S]*?(?=\n.*?(<option)|$)/g, ""); // trim out other messages that it confuses for candidate names
+        
         var dumpLines = dumptext.splitNewline();  // trimming out the actual votes and candidates
         $(dumpLines).each(function (index) {
-            if (this.match(candidateStart)) {
+            if (this.match(lang)) {
                 trimHold = dumpLines[index]
-                    .replace(/<message.*lang=\"\w\w\">/g, "") // this assumes non-English, will need to be reworked for an election that does not specify language
+                    .replace(lang, "")
                     .replace(/<\/message>/g, "");
                 candidates.push(trimHold);
             } else if (this.match(voteStart)) {
@@ -47,9 +54,6 @@ $(document).ready(function () {
                 votes.push(trimHold);
             }
         });
-        for (i=0;i<4;i++) { // trim out the other params
-            candidates.shift();
-        }
         $(votes).each(function(index) {
             var j = votes[index].split(/--/).slice(0, -1);
             console.log(j);
@@ -65,24 +69,38 @@ $(document).ready(function () {
             finalVotes.push(0); //init
         });
         
-        var t = 0;
-        $(voters).each(function(index) {
-            t = voters[index].reduce((a, b) => a + b, 0);
-            if(t == candidateAmount) {
+        var t = 0; //init
+        
+        if (mustVote.checked) { // checking to make sure we need this requirement, otherwise we'll just include all votes
+            $(voters).each(function(index) {
+                t = voters[index].reduce((a, b) => a + b, 0); // 
+                console.log(t);
+                if(t == candidateAmount) {
+                    for(var k=0; k<voters[index].length; k++) {
+                        if(voters[index][k] == 1) {
+                            finalVotes[k]++;
+                        }
+                    }
+                }
+                if (index > 0) { //disregard the first "voter" which is currently blank. if this is fixed later remember to remove this if statement
+                    if (t !== candidateAmount) {
+                        $('#votes-list').append("<span class='ineligible'>Voter " + index + " voted for " + t + " candidates, so their vote was not counted.</span><br />");
+                    } else {
+                        $('#votes-list').append("<span class='eligible'>Voter " + index + " voted for " + t + " candidates.</span><br />");
+                    }
+                }
+            });
+        } else {
+            $(voters).each(function(index) {
+                t = voters[index].reduce((a, b) => a + b, 0); // 
+                console.log("Voter " + index + " voted for " + t + " candidates");
                 for(var k=0; k<voters[index].length; k++) {
                     if(voters[index][k] == 1) {
                         finalVotes[k]++;
                     }
                 }
-            }
-            if (index > 0) { //disregard the first "voter" which is currently blank. if this is fixed later remember to remove this if statement
-                if (t !== candidateAmount) {
-                    $('#votes-list').append("<span class='ineligible'>Voter " + index + " voted for " + t + " candidates, so their vote was not counted.</span><br />");
-                } else {
-                    $('#votes-list').append("<span class='eligible'>Voter " + index + " voted for " + t + " candidates.</span><br />");
-                }
-            }
-        });
+            });
+        }
         
         for (var u=0; u < candidates.length; u++) { // combining into one array for sorting purposes
             standings.push({'candidate': candidates[u], 'votes': finalVotes[u]});
@@ -91,8 +109,10 @@ $(document).ready(function () {
            return ((a.votes > b.votes) ? -1 : ((a.votes == b.votes) ? 0 : 1));
         });
         
-        if (standings[candidateAmount-1].votes == standings[candidateAmount].votes) { // work out if candidates n and n+1 have the same number of votes, thus necessitating a recount
-            $('#candidate-list').append("<b>Looks like a revote is needed.</b> " + standings[candidateAmount-1].candidate + " (" + candidateAmount + "th place) and " + standings[candidateAmount].candidate + " (" + parseInt(candidateAmount+1) + "th place) both have " + standings[candidateAmount].votes + " votes.<br /><br />")
+        if (mustVote.checked) {
+            if (standings[candidateAmount-1].votes == standings[candidateAmount].votes) { // work out if candidates n and n+1 have the same number of votes, thus necessitating a recount
+                $('#candidate-list').append("<b>Looks like a revote is needed.</b> " + standings[candidateAmount-1].candidate + " (" + candidateAmount + "th place) and " + standings[candidateAmount].candidate + " (" + parseInt(candidateAmount+1) + "th place) both have " + standings[candidateAmount].votes + " votes.<br /><br />")
+            }
         }
         
         var html = "<ol>";
